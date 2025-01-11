6669ea73509ce3e06950db6c10e7ff0f3bf50743
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2025-01-11 21:46:05 +0700

    add webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,61 +1,50 @@
-import { createUser } from "@/lib/actions/user.actions";
-import { WebhookEvent } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import { Webhook } from "svix";
+import { Webhook } from 'svix'; // Đảm bảo bạn đã cài đặt và nhập đúng thư viện svix
+import { NextResponse } from 'next/server';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { createUser } from '@/lib/actions/user.actions';
 
-const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
 
 export async function POST(req: Request) {
-  const svix_id = req.headers.get("svix-id") ?? "";
-  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
   const svix_signature = req.headers.get("svix-signature") ?? "";
-if (!webhookSecret) {
+  const webhookSecret = process.env.WEBHOOK_SECRET; // Đảm bảo bạn đã thiết lập biến môi trường này
+
+  if (!webhookSecret) {
     return new Response("Internal Server Error", { status: 500 });
-}
+  }
+
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  const sivx = new Webhook(webhookSecret);
+  const svix = new Webhook(webhookSecret);
 
   let msg: WebhookEvent;
-  
+
   try {
-    msg = sivx.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
+    msg = svix.verify(body, {
+      "svix-id": req.headers.get("svix-id") ?? "",
+      "svix-timestamp": req.headers.get("svix-timestamp") ?? "",
       "svix-signature": svix_signature,
     }) as WebhookEvent;
+  } catch (e) {
+    console.error("Error verifying webhook:", e); // Ghi lại lỗi
+    return new Response("Unauthorized", { status: 401 });
   }
-    catch (e) {
-        console.error("Error verifying webhook:", e); // Ghi lại lỗi
-        return new Response("Unauthorized", { status: 401 });
-    }
 
   const eventType = msg.type;
-  if (eventType !== "user.created") {
-    const {
-      id,
-      username,
-      email_address,
-      image_url
-        } = msg.data;
-    const user = await createUser(
-        {
-            clerkId: id!,
-            username: username!,
-            email: email_address[0].email_address,
-            avatar: image_url,
-            name: username!
-            
-        }
-    );
+  if (eventType === "user.created") {
+    const { id, username, email_addresses, image_url } = msg.data;
+    const user = await createUser({
+      clerkId: id!,
+      username: username!,
+      email: email_addresses[0].email_address,
+      avatar: image_url,
+      name: username!,
+    });
     return NextResponse.json({
       message: "Ok",
       user,
     });
   }
 
-  // Rest
-
-  return new Response("OK", { status: 200 });
+  return new Response("Event type not handled", { status: 400 });
 }
