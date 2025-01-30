f813bef684338982995a1dac5bcdb43ce0c6e08f
hoangthach.dev <nguyenhoangthach2005@gmail.com>
2025-01-30 12:18:26 +0700

    feat: Refactor course management page component

--- src/app/(dashboard)/admin/manage/courses/page.tsx
+++ src/app/(dashboard)/admin/manage/courses/page.tsx
@@ -19,33 +19,32 @@ export default function CourseManagement() {
   const [courses, setCourses] = useState<TCourseInfo[]>([])
   const [selectedCourse, setSelectedCourse] = useState<string>("")
   const [selectedChapter, setSelectedChapter] = useState<string | undefined>("")
-  const [lessons, setLessons] = useState<TLesson[]|undefined>([])
+  const [lessons, setLessons] = useState<TLesson[] | undefined>([])
   const [editLesson, setEditLesson] = useState<boolean>(false)
   const [lessonToEdit, setLessonToEdit] = useState<TEditLesson | null>(null)
   const [newChapterTitle, setNewChapterTitle] = useState("")
-  const [chapters, setChapters] = useState<TCreateLecture[]|undefined>([])
+  const [chapters, setChapters] = useState<TCreateLecture[] | undefined>([])
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
-  } = useForm();
+  } = useForm<TCreateLesson>()
   const [refreshTrigger, setRefreshTrigger] = useState(0)
 
   useEffect(() => {
     const fetchData = async () => {
       const course = await getCourses()
-      const chapter = await getChapter(selectedCourse!)
-      const lesson = await getLessons(selectedChapter!,selectedCourse )
-      
+      const chapter = await getChapter(selectedCourse)
+      const lesson = await getLessons(selectedChapter!, selectedCourse)
+
       setLessons(lesson || [])
       setChapters(chapter || [])
       setCourses(course || [])
-      
     }
     fetchData()
   }, [refreshTrigger, selectedCourse])
-  
+
   const addChapter = async () => {
     try {
       const data: TCreateLecture = {
@@ -53,7 +52,7 @@ export default function CourseManagement() {
         course: new mongoose.Types.ObjectId(selectedCourse),
         deleted: false
       }
-      
+
       await createLecture(data)
       setRefreshTrigger(prev => prev + 1) // Trigger re-fetch
       setNewChapterTitle("") // Optional: Clear input
@@ -62,6 +61,7 @@ export default function CourseManagement() {
       console.log("lỗi nè", error)
     }
   }
+
   const getChapter = async (selectedCourse: string) => {
     try {
       console.log("selectedCourse", selectedCourse)
@@ -72,7 +72,7 @@ export default function CourseManagement() {
       console.log("lỗi nè", error)
     }
   }
-  
+
   const handleDeleteLecture = async (id: string) => {
     await deleteLecture(id)
     setRefreshTrigger(prev => prev + 1) // Trigger re-fetch
@@ -91,29 +91,25 @@ export default function CourseManagement() {
       content: "",
       type: "",
       deleted: false
-    };
-    setLessonToEdit(editLesson);
-    setEditLesson(true);
-  };
-
-  
+    }
+    setLessonToEdit(editLesson)
+    setEditLesson(true)
+  }
 
   const handleCloseDialog = () => {
-
-    setEditLesson(false);
-    setLessonToEdit(null);
+    setEditLesson(false)
+    setLessonToEdit(null)
     setRefreshTrigger(prev => prev + 1)
-  };
+  }
 
   const onSubmit = async (data: TCreateLesson) => {
-    data.course = new mongoose.Types.ObjectId(selectedCourse);
-    data.lecture = new mongoose.Types.ObjectId(selectedChapter);
+    data.course = new mongoose.Types.ObjectId(selectedCourse)
+    data.lecture = new mongoose.Types.ObjectId(selectedChapter)
     console.log("course", selectedCourse)
     console.log("chapter", selectedChapter)
-    await createLesson(data);
-    reset(); // Reset form sau khi submit thành công
-  };
-
+    await createLesson(data)
+    reset() // Reset form sau khi submit thành công
+  }
 
   return (
     <div className="container mx-auto p-4">
@@ -130,7 +126,6 @@ export default function CourseManagement() {
             <TableHead>Price</TableHead>
             <TableHead>Sale Price</TableHead>
             <TableHead>Status</TableHead>
-        
             <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
@@ -142,7 +137,6 @@ export default function CourseManagement() {
               <TableCell>${course.price}</TableCell>
               <TableCell>${course.sale_price}</TableCell>
               <TableCell>{course.status}</TableCell>
-              
               <TableCell className="flex gap-2">
                 <Dialog>
                   <DialogTrigger asChild>
@@ -168,51 +162,45 @@ export default function CourseManagement() {
                           <AccordionItem key={chapter._id || chapterIndex} value={chapter._id || chapterIndex.toString()}>
                             <AccordionTrigger className="flex relative" onClick={() => {
                               setSelectedChapter(chapter._id)
-                              setRefreshTrigger(prev => prev+1)
-                              }}>
+                              setRefreshTrigger(prev => prev + 1)
+                            }}>
                               <span className="">{chapter.title}</span>
-                             
                               <div className="absolute right-0 flex items-center space-x-2">
                                 <Button variant="outline" size="icon" className="h-8 w-8">
                                   <Edit className="h-4 w-4" />
                                 </Button>
-                                <Button variant="destructive" size="icon" className="h-8 w-8" onClick={()=> handleDeleteLecture(chapter._id ?? '')}>
+                                <Button variant="destructive" size="icon" className="h-8 w-8" onClick={() => handleDeleteLecture(chapter._id ?? '')}>
                                   <Trash2 className="h-4 w-4" />
                                 </Button>
                               </div>
                             </AccordionTrigger>
-                            
                             <AccordionContent>
-                            <form onSubmit={handleSubmit(onSubmit)}  className="flex space-x-2 mb-4">
-                                  <Input
-                                    placeholder="New Lesson Title"
-                                    {...register("title", { required: "Lesson Title is required" })}
-                                    className="flex-grow"
-                                  />
-                                  {errors.title && (
-                                    <p className="text-red-500 text-sm">{String(errors.title.message)}</p>
-                                  )}
-
-                                  {/* Input: Lesson Slug */}
-                                  <Input
-                                    placeholder="New Lesson Slug"
-                                    {...register("slug", { required: "Lesson Slug is required" })}
-                                    className="flex-grow"
-                                  />
-                                  {errors.slug && (
-                                    <p className="text-red-500 text-sm">{String(errors.slug.message)}</p>
-                                  )}
-
-                                 
-                                  <Button type="submit" className="shrink-0" onClick={()=>{setRefreshTrigger(prev => prev+1)}}>
-                                    <Plus className="mr-2 h-4 w-4" /> Add Lesson
-                                  </Button>
-                                </form>
+                              <form onSubmit={handleSubmit(onSubmit)} className="flex space-x-2 mb-4">
+                                <Input
+                                  placeholder="New Lesson Title"
+                                  {...register("title", { required: "Lesson Title is required" })}
+                                  className="flex-grow"
+                                />
+                                {errors.title && (
+                                  <p className="text-red-500 text-sm">{String(errors.title.message)}</p>
+                                )}
+                                <Input
+                                  placeholder="New Lesson Slug"
+                                  {...register("slug", { required: "Lesson Slug is required" })}
+                                  className="flex-grow"
+                                />
+                                {errors.slug && (
+                                  <p className="text-red-500 text-sm">{String(errors.slug.message)}</p>
+                                )}
+                                <Button type="submit" className="shrink-0" onClick={() => { setRefreshTrigger(prev => prev + 1) }}>
+                                  <Plus className="mr-2 h-4 w-4" /> Add Lesson
+                                </Button>
+                              </form>
                               {lessons && lessons.length > 0 ? (
                                 <ul className="space-y-2">
                                   {lessons.map((lesson, lessonIndex) => (
-                                    <li 
-                                      key={lesson._id || lessonIndex} 
+                                    <li
+                                      key={lesson._id || lessonIndex}
                                       className="flex justify-between items-center p-2 bg-gray-100 rounded"
                                     >
                                       <span>{lesson.title}</span>
@@ -220,7 +208,7 @@ export default function CourseManagement() {
                                         <Button variant="outline" size="icon" className="h-7 w-7" onClick={() => handleEditLesson(lesson)}>
                                           <Edit className="h-4 w-4" />
                                         </Button>
-                                        <Button variant="destructive" size="icon" className="h-7 w-7" onClick={()=>handleDeleteLesson(lesson._id)}>
+                                        <Button variant="destructive" size="icon" className="h-7 w-7" onClick={() => handleDeleteLesson(lesson._id)}>
                                           <Trash2 className="h-4 w-4" />
                                         </Button>
                                       </div>
@@ -248,14 +236,11 @@ export default function CourseManagement() {
           ))}
         </TableBody>
       </Table>
-      
-       
-    <EditLessonDialog 
-      editLesson={editLesson} 
-      lesson={lessonToEdit} 
-      onClose={handleCloseDialog} 
-     
-    />
+      <EditLessonDialog
+        editLesson={editLesson}
+        lesson={lessonToEdit}
+        onClose={handleCloseDialog}
+      />
     </div>
   )
 }
